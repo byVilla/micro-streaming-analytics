83f20d8d7c43eb141d4069341afaa8e315c4919a
E1000\30035989 <raul.munoz@ext.leroymerlin.es>
2019-06-14 14:11:57 +0200

    Adding scheduled method in consumer class.

--- src/main/java/es/amplia/streaming/analytics/consumer/MessageConsumer.java
+++ src/main/java/es/amplia/streaming/analytics/consumer/MessageConsumer.java
@@ -2,16 +2,36 @@ package es.amplia.streaming.analytics.consumer;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-@RabbitListener(queues = "${principal.queue.name")
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class MessageConsumer {
 
 	private static Logger logger = LogManager.getLogger(MessageConsumer.class);
+	@Value(value = "${default.listener.method.name}")
+	private String defaultListenerMethodName;
 
-	@RabbitHandler
-	public void receive(String message) {
-		logger.info("message received: s%", message);
+	private List<Object> messageList = new ArrayList();
+
+	public String getDefaultListenerMethodName() {
+		return defaultListenerMethodName;
+	}
+
+	public void receiveMessage(String message) {
+		messageList.add(message);
+		logger.info("message received: {}", message);
+	}
+
+	@Scheduled(fixedDelayString = "${processing.fixedDelay.in.milliseconds}")
+	private void processingMessageList() {
+		List copy = messageList.stream().collect(Collectors.toList());
+		messageList.clear();
+		copy.forEach(msg -> logger.info("Message {}", msg));
 	}
 }
