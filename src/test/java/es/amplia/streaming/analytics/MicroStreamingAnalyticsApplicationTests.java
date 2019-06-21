397601b79874aaec40f5209086fe0a754e67fa7a
E1000\30035989 <raul.munoz@ext.leroymerlin.es>
2019-06-22 01:35:35 +0200

    Tests configuration

--- src/test/java/es/amplia/streaming/analytics/MicroStreamingAnalyticsApplicationTests.java
+++ src/test/java/es/amplia/streaming/analytics/MicroStreamingAnalyticsApplicationTests.java
@@ -2,11 +2,15 @@ package es.amplia.streaming.analytics;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableRabbit
+@ActiveProfiles(value = {"test"})
 public class MicroStreamingAnalyticsApplicationTests {
 
 	@Test
